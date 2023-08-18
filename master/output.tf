137942a1c71e49e223b1e39ee74d1c3f8b5b6f11
Chloe Wu <Chloe.wu22@imperial.ac.uk>
2023-08-18 17:37:33 +0100

    output changed

--- master/output.tf
+++ master/output.tf
@@ -1,53 +1,37 @@
-/*
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config[0].client_certificate
-  sensitive = true
-}
-
-output "client_key" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config[0].client_key
-  sensitive = true
-}
-
-output "cluster_ca_certificate" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config[0].cluster_ca_certificate
-  sensitive = true
-}
-*/
 
 output "cluster_password" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config[0].password
+  value     = azurerm_kubernetes_cluster.sandbox_aks.kube_config[0].password
   sensitive = true
 }
 
 output "cluster_username" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config[0].username
+  value     = azurerm_kubernetes_cluster.sandbox_aks.kube_config[0].username
   sensitive = true
 }
 
 output "host" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config[0].host
+  value     = azurerm_kubernetes_cluster.sandbox_aks.kube_config[0].host
   sensitive = true
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.clearly_aks.kube_config_raw
+  value     = azurerm_kubernetes_cluster.sandbox_aks.kube_config_raw
   sensitive = true
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.clearly-earth-rg.name
+  value = azurerm_resource_group.sandbox-cadt-rg.name
 }
 
 # Additional outputs from AKS 
 output "cluster_id" {
-  value = azurerm_kubernetes_cluster.clearly_aks.id
+  value = azurerm_kubernetes_cluster.sandbox_aks.id
 }
 output "cluster_name" {
-  value = azurerm_kubernetes_cluster.clearly_aks.name
+  value = azurerm_kubernetes_cluster.sandbox_aks.name
 }
 output "cluster_kubernetes_version" {
-  value = azurerm_kubernetes_cluster.clearly_aks.kubernetes_version
+  value = azurerm_kubernetes_cluster.sandbox_aks.kubernetes_version
 }
 
 # PBV Output ACR info
